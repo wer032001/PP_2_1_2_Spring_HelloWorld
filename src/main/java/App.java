7a626db05cb043c0b5ffced6c75b4fb1f4239f09
wer032001 <wer032001@gmail.com>
2023-01-15 20:32:30 +0300

    add class Cat, add HelloWorld2, compare Cat-Cat2, HelloWorld-HelloWorld2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorld.getMessage());
+
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorld2.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+
+        System.out.println(helloWorld == helloWorld2);
+
+        System.out.println(cat == cat2);
+
     }
 }
